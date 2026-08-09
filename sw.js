d7bb0326e3f96689618381674f4338a91dd23901
francescogaeta <francescogaeta.gaetafrancesco@gmail.com>
2026-08-09 11:03:12 +0200

    Update cache version to 'polis-v2.2'

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='polis-v1.2';
+const CACHE='polis-v2.2';
 self.addEventListener('install',e=>self.skipWaiting());
 self.addEventListener('activate',e=>e.waitUntil(clients.claim()));
 self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(k=>k.put(e.request,c));return r;}).catch(()=>caches.match(e.request)));});
